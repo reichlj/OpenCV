4f45cfb762d781048920cddaf8833892f3d881b3
reichlj <jakobreichl@gmail.com>
2021-05-11 15:51:42 +0200

    Templatematching

--- OpenCVPysource/20TemplateMatching.py
+++ OpenCVPysource/20TemplateMatching.py
@@ -6,11 +6,12 @@ gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 template = cv2.imread('images/barts_face.jpg',cv2.IMREAD_GRAYSCALE)
 h, w = template.shape
 result = cv2.matchTemplate(gray_img,template,cv2.TM_CCOEFF_NORMED)
+# loc ist Tupel von zwei Vektoren ( zeilenindices, spaltenindices )
 loc = np.where(result >= 0.9)  #(r,c)
 
 #for pt in zip(*loc[::-1]):
 #   cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,255,0),3)
-for (r,c) in zip(*loc):
+for (r,c) in zip(*loc):  # invert order
    cv2.rectangle(img,(c,r),(c+w,r+h),(0,255,0),3)
 
 cv2.imshow("img", img)
